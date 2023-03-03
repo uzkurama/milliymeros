4d0bc8a342d11915b08c7c8a9bf7e30d82a86b95
Amir <uz.kurama@gmail.com>
2023-03-03 19:14:44 +0500

    Update db.php

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=milliy',
-    'username' => 'kurama',
-    'password' => 'kurama',
+    'username' => '',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
